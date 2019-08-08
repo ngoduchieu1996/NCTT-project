771dbcf51050163291f66ed5e52ec06a20737e6f
Nguyen Duy Quang <nguyenduyquang93@gmail.com>
2019-08-08 14:45:45 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+// owlCarousel
+
+$('#partner-page .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:4
+        },
+        600:{
+            items:3
+        },
+        767:{
+            items:5
+        },
+        992:{
+            items:5
+        }
+    }
+});
+$('#news-main-2 .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        767:{
+            items:2
+        },
+        992:{
+            items:3
+        }
+    }
+});
+$('#in-the-media .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        767:{
+            items:3
+        },
+        992:{
+            items:3
+        }
+    }
+});
+
+
